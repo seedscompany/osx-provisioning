08104aa97c21fff1cdf6566e7d0ee9e7833d2225
ntk1000 <jun22011982@gmail.com>
2016-10-28 16:24:51 +0900

    [fix] ansible version tag
    detail here

--- Context.sh
+++ Context.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 PROVISIONING_HOME=$(cd $(dirname $0) && pwd)
-ANSIBLE_VERSION_MAJOR=$(echo $(ansible --version) | sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+).*/\1/g" | cut -d"." -f1)
+ANSIBLE_VERSION=$(echo $(ansible --version) | cut -d" " -f2)
 
 function show_context() {
   echo "PROVISIONING_HOME=${PROVISIONING_HOME}"
-  echo "ANSIBLE_VERSION_MAJOR=${ANSIBLE_VERSION_MAJOR}"
+  echo "ANSIBLE_VERSION=${ANSIBLE_VERSION}"
 }
+
+show_context
